863dad1bdebd7d6201df3d6c358aa6aec222e248
Fabiano Góes <fabiano@cora.com.br>
2022-03-21 21:21:25 -0300

    GoHorse implementation

---
+++ src/main/kotlin/com/demo/pedido/Controllers.kt
@@ -0,0 +1,31 @@
+package com.demo.pedido
+
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("order")
+class Controllers {
+
+    @PostMapping
+    fun post(@RequestBody dto: OrderDTO): ResponseEntity<Order> {
+        val customerRepository = CustomerRepository()
+        val customer = customerRepository.findByCpf(dto.cpf)
+
+        val productRepository = ProductRepository()
+        val listOfProducts = productRepository.findByCodes(dto.productCodes)
+
+        val creditCardRepository = CreditCardRepository()
+        val card = creditCardRepository.findCardByCustomer(customer)
+
+        val order = Order(customer, listOfProducts, card)
+        val orderRepository = OrderRepository()
+        val orderSaved = orderRepository.finalizeOrder(order)
+
+        return ResponseEntity.ok(orderSaved)
+    }
+
+}
---
+++ src/main/kotlin/com/demo/pedido/DTOs.kt
@@ -0,0 +1,6 @@
+package com.demo.pedido
+
+class OrderDTO(
+    val cpf: String,
+    val productCodes: List<String>
+)
