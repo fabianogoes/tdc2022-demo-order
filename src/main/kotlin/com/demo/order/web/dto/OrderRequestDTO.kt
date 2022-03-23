64fa6112cdfc04a11c9357164956ce0ddacdfd5d
Fabiano Góes <fabiano@cora.com.br>
2022-03-23 16:12:17 -0300

    refactor dto

--- src/main/kotlin/com/demo/order/web/dto/OrderRequestDTO.kt
+++ src/main/kotlin/com/demo/order/web/dto/OrderRequestDTO.kt
@@ -1,26 +1,6 @@
 package com.demo.order.web.dto
 
-import com.demo.order.domain.model.Order
-import java.util.UUID
-
 class OrderRequestDTO(
     val cpf: String,
     val productCodes: List<String>
 )
-
-class OrderResponseDTO(
-    val id: UUID,
-    val state: OrderStateDTO
-) {
-    enum class OrderStateDTO {
-        CREATED,
-        REQUEST_PAYMENT,
-        COMPLETED
-    }
-}
-
-fun Order.toResponse() =
-    OrderResponseDTO(
-        id = this.id!!,
-        state = enumValueOf(this.state.name)
-    )
